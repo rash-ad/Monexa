13d0cc63c267e9360f45af2a087aa2300eb55795
rash-ad <Rashad.mohd2004@gmail.com>
2026-05-07 12:52:47 +0530

    feat: add AccountResponseDTO

--- src/main/java/edu/icet/ecom/dto/Account/AccountResponseDTO.java
+++ src/main/java/edu/icet/ecom/dto/Account/AccountResponseDTO.java
@@ -1,4 +1,14 @@
 package edu.icet.ecom.dto.Account;
 
+import lombok.Data;
+
+@Data
 public class AccountResponseDTO {
+    private Long id;
+    private String accountNumber;
+    private String accountType;
+    private String balance;
+    private String currency;
+    private String status;
+    private String createdAt;
 }
